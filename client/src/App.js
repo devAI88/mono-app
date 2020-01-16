32ef1980ad99f0c9452dc73bde4365ec7b112a58
AbdelI <homeworkoracle@protonmail.com>
2020-01-16 11:58:27 +0100

    gretting message

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [greeting, setGreeting] = useState("");
+
+  useEffect(() => {
+    fetch("api/greeting?name=cool")
+      .then(res => {
+        res.json();
+      })
+      .then(greeting => setGreeting(greeting))
+      .catch(err => {});
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">{greeting}</header>
     </div>
   );
 }
